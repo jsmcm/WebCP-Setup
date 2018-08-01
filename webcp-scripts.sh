a9e11d20329e2cc3cbfb8b6a3515d741aec8b788
root <root@ubuntu.demoserver.co.za>
2018-08-01 15:38:04 +0200

    1) updated webcp www/scripts scrips to use git instead of wget 2) Initial release

--- webcp-scripts.sh
+++ webcp-scripts.sh
@@ -3,12 +3,13 @@
 mkdir -p /usr/webcp
 cd /usr/webcp
 
-wget http://api.webcp.pw/downloads/2.0.0/scripts.zip
-unzip scripts.zip
+##wget http://api.webcp.pw/downloads/2.0.0/scripts.zip
+##unzip scripts.zip
 
+git clone http://api.webcp.pw/downloads/2.0.0/git/scripts.git ./
 chmod 755 /usr/webcp -R
 
-rm -fr /usr/webcp/scripts.zip
+##rm -fr /usr/webcp/scripts.zip
 
 
 
--- webcp-www.sh
+++ webcp-www.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-cd /var/www/html/
-wget http://api.webcp.pw/downloads/2.0.0/webcp.zip
-unzip webcp.zip
+mkdir -p /var/www/html/webcp
+cd /var/www/html/webcp
+git clone http://api.webcp.pw/downloads/2.0.0/git/webcp.git ./
 chown www-data.www-data /var/www/html/webcp -R
 
 
+cd /var/www/html/
 wget http://api.webcp.pw/downloads/2.0.0/editor.zip
 unzip editor.zip
 chown www-data.www-data /var/www/html/editor -R
 
-rm -fr /var/www/html/webcp.zip
 rm -fr /var/www/html/editor.zip
 
 cd /etc/skel
