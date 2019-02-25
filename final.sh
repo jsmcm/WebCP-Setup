145252391dc584df172194b30e230b0bb13054ac
John Mc <john@softsmart.co.za>
2019-02-25 15:31:05 +0100

    added making nm directory so that we can touch letsencrypt install file

--- final.sh
+++ final.sh
@@ -5,4 +5,5 @@ echo "* * * * * /usr/webcp/director.sh > /dev/null 2>&1" | crontab
 cd /var/www/html/webcp
 composer install
 
+mkdir /var/www/html/webcp/nm
 touch /var/www/html/webcp/nm/letsencrypt.install
