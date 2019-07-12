bb9c003d7fdc2cf5d5aaae69767ce94b4a23587a
John Mc <john@softsmart.co.za>
2019-07-12 20:45:04 +0200

    Added php5.6 installer

--- php56.sh
+++ php56.sh
@@ -7,16 +7,16 @@
 #
 ##################
 
-apt install software-properties-common
+apt install software-properties-common -y
 add-apt-repository ppa:ondrej/php
-apt update
+apt update -y
 
 apt-get install php5.6-fpm php5.6-mysql php5.6-xml php5.6-gd php5.6-zip php5.6-curl php5.6-mbstring  php5.6-xml php5.6-soap php5.6-imap php5.6-opcache -y
 apt-get install php5.6-pear -y
 
 apt-get install php5.6-intl php5.6-mail-mime php5.6-mail-mimedecode php5.6-net-smtp php5.6-net-socket php5.6-cli php5.6-common php5.6-curl php5.6-fpm php5.6-gd php5.6-imap php5.6-intl php5.6-json php5.6-mbstring php5.6-mcrypt php5.6-mysql php5.6-opcache php5.6-readline php5.6-soap php5.6-xml php5.6-zip -y
 
-apt-get install php5.6-mcrypt php5.6-opcache php5.6-auth
+apt-get install php5.6-mcrypt php5.6-opcache php5.6-auth -y
 
 
 
